87554c6561325938617087fe0490d5e1ec033511
ali <250950@astanait.edu.kz>
2025-12-25 14:48:55 +0500

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,22 @@
 public class Main {
-
     public static void main(String[] args) {
 
-        System.out.println("=== Movie Ticket Reservation System ===\n");
+        Cinema cinema = new Cinema(5);
 
-        Film f1 = new Film("Avatar");
-        Film f2 = new Film("Titanic");
-        Viewer v = new Viewer("Ali");
-        Cinema c = new Cinema("Chaplin");
+        cinema.addFilm(new Film("Avatar", 180));
+        cinema.addFilm(new Film("Titanic", 195));
+        cinema.addFilm(new Film("Up", 96));
 
-        System.out.println("Created classes: Film, Viewer, Cinema -> DONE");
-        System.out.println("Film 1 title: " + f1.getTitle());
-        System.out.println("Film 2 title: " + f2.getTitle());
+        Viewer viewer = new Viewer("Ali", 20);
+        viewer.printInfo();
 
-        System.out.println("Viewer name: " + v.getName());
-        System.out.println("Cinema name: " + c.getName());
+        System.out.println("\nAll films:");
+        cinema.showAllFilms();
 
-        System.out.println("Object comparison (films): " +
-                (f1.getTitle().equals(f2.getTitle())
-                        ? "Same films"
-                        : "Different films"));
-    }
-}
+        System.out.println("\nFilms longer than 150 minutes:");
+        cinema.showLongFilms(150);
 
-class Film {
-    private String title;
-
-    public Film(String title) {
-        this.title = title;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-}
-
-class Viewer {
-    private String name;
-
-    public Viewer(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-}
-
-class Cinema {
-    private String name;
-
-    public Cinema(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
+        System.out.println("\nSearch film:");
+        cinema.findFilm("Avatar");
     }
 }
